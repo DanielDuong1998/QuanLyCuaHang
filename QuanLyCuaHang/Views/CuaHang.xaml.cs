938abe8e41296b367b1014839d13bae2b2ff898a
= <dvkhangnt@gmail.com>
2019-06-03 18:01:58 +0700

    first commit

---
+++ QuanLyCuaHang/Views/CuaHang.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using WpfPageTransitions;
+
+namespace QLCuaHang.Views
+{
+    public partial class CuaHang : Window
+    {
+ 
+        public CuaHang()
+        {
+            InitializeComponent();           
+         
+        }
+
+        private void Btnphucvu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click roi nè");
+            PageHome phucvu = new PageHome();
+            pageTransitionControl.ShowPage(phucvu);
+
+        }
+    }
+}
