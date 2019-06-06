e95c02885f4735b95d56c56e4f07856f67cf3612
unknown <you@example.com>
2019-06-06 22:17:23 +0700

    them dal

---
+++ QuanLyCuaHang/Dal/HoaDonDal.cs
@@ -0,0 +1,136 @@
+﻿using QuanLyCuaHang.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang.Dal
+{
+    public class HoaDonDal
+    {
+        WPF_DatabaseEntities db = new WPF_DatabaseEntities();
+        public List<HOADON> sp_loaddanhsachthucdoncuaban(int maban, int mahoadon)
+        {
+            //int parameter = 2;
+            //string[] name = new string[parameter];
+            //object[] values = new object[parameter];
+            //name[0]= "@maban";
+            //name[1]= "@mahoadon";
+            //values[0]=maban;
+            //values[1]=mahoadon;
+            //string json = JsonConvert.SerializeObject(LoadDataParameter("sp_loaddanhsachthucdoncuaban", name, values, parameter));
+            //return JsonConvert.DeserializeObject<List<HoaDonModels>>(json);
+            List<HOADON> list = new List<HOADON>();
+            list = db.HOADONs.Where(x=>x.MABAN==maban && x.IDHOADON==mahoadon).ToList();
+            return list;
+        }
+        public List<HOADON> sp_layidhoadontheo_khuvucban(int maban, int makhuvuc)
+        {
+            /*int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@maban";
+            name[1] = "@makhuvuc";
+            values[0] = maban;
+            values[1] = makhuvuc;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_layidhoadontheo_khuvucban", name, values, parameter));
+            
+            return JsonConvert.DeserializeObject<List<HoaDonModels>>(json);*/
+            var query = from hoadon in db.HOADONs
+                        join ban in db.BANs on hoadon.MABAN equals ban.MABAN
+                        where hoadon.MABAN==maban && ban.MAKHUVUC==makhuvuc
+                        select hoadon;
+            List <HOADON> list = new List<HOADON>();
+            //list = db.HOADONs.Where(x => x.MABAN == maban).Join();
+            list = query.ToList();
+            return list;
+        }
+        public bool sp_themhoadon( int maban)
+        {
+            /* int parameter = 2;
+             string[] name = new string[parameter];
+             object[] values = new object[parameter];
+             name[0] = "@ngaylap";
+             name[1] = "@maban";       
+             values[0] = ngaylap;
+             values[1] = maban;        
+             return Execute("sp_themhoadon", name, values, parameter);*/
+            var ngaylap = DateTime.Now;
+            HOADON hd = new HOADON(ngaylap, maban);
+            db.HOADONs.Add(hd);
+            db.SaveChanges();
+            return true;
+        }
+        public bool sp_themcthdban(int idhoadon, int mathucdon, int soluong,int giamgia)
+        {
+            /* int parameter = 4;
+             string[] name = new string[parameter];
+             object[] values = new object[parameter];
+             name[0]= "@idhoadon";
+             name[1]= "@mathucdon";
+             name[2]= "@soluong";
+             name[3] = "@giamgia";
+             values[0]= idhoadon;
+             values[1]= mathucdon;
+             values[2]= soluong;
+             values[3] = giamgia;
+             return Execute("sp_themcthdban", name, values, parameter);*/
+            CTHD ct = new CTHD(idhoadon, mathucdon, soluong, giamgia);
+            db.CTHDs.Add(ct);
+            db.SaveChanges();
+            return true;
+        }
+        public bool sp_thanhtoanban(int maban, int mahoadon, double tongtien,string idnhanvien)
+        {
+            /*int parameter = 4;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@maban";
+            name[1]= "@mahoadon";
+            name[2]= "@tongtien";
+            name[3] = "@idnhanvien";
+            values[0]=maban;
+            values[1]=mahoadon;
+            values[2]= tongtien;
+            values[3] = idnhanvien;
+            return Execute("sp_thanhtoanban", name,values,parameter);*/
+            var hd = db.HOADONs.Where(x => x.MABAN == maban).FirstOrDefault();
+            hd.IDHOADON = mahoadon;
+            hd.TONGTIEN = tongtien;
+            hd.IDNHANVIEN = idnhanvien;
+            db.SaveChanges();
+            return true;
+        }
+        //public DataTable sp_loaddanhsachthucdoncuaban_rpt(int maban, int mahoadon, string giora)
+        //{
+        //    /*int parameter = 3;
+        //    string[] name = new string[parameter];
+        //    object[] values = new object[parameter];
+        //    name[0] = "@maban";
+        //    name[1] = "@mahoadon";
+        //    name[2] = "@giora";
+        //    values[0] = maban;
+        //    values[1] = mahoadon;
+        //    values[2] = giora;
+        //    return LoadDataParameter("sp_loaddanhsachthucdoncuaban_rpt", name, values, parameter);*/
+
+        //}
+        /*public bool sp_capnhathoadondain(int idhoadon)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@idhoadon";         
+            values[0] = idhoadon;          
+            return Execute("sp_capnhathoadondain", name, values, parameter);
+        }
+        public List<HoaDonModels> sp_loadhoadoncanin()
+        {
+            string json = JsonConvert.SerializeObject(LoadData("sp_loadhoadoncanin"));
+            return JsonConvert.DeserializeObject<List<HoaDonModels>>(json);
+        }*/
+    }
+
+}
---
+++ QuanLyCuaHang/Dal/KhuVucBanDal.cs
@@ -0,0 +1,191 @@
+﻿using QuanLyCuaHang.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CafeMVVM.Dal
+{
+    public class KhuVucBanDal
+    {
+        WPF_DatabaseEntities db = new WPF_DatabaseEntities();
+        public List<KHUVUC> sp_loadkhuvuc()
+        {
+            List<KHUVUC> kv = new List<KHUVUC>();
+            kv = db.KHUVUCs.ToList();
+            return kv;           
+        }
+        public List<KHUVUC> sp_loadbantheokhuvuc(int makhuvuc)
+        {
+            List<KHUVUC> kv = new List<KHUVUC>();
+            kv = db.KHUVUCs.Where(x=>x.MAKHUVUC==makhuvuc).ToList();
+            return kv;
+        }
+        public List<KHUVUC> sp_loadkhuvuc_danhmuc()
+        {
+            List<KHUVUC> kv = new List<KHUVUC>();
+            var query = from kvs in db.KHUVUCs
+                        join dm in db.BANs on kvs.MAKHUVUC equals dm.MAKHUVUC
+                        
+                        select new {kvs.MAKHUVUC, kvs.TENKHUVUC,dm.MABAN};
+            var t = from p in query
+                    group p by p.MAKHUVUC into g
+                    select new Result()
+                    {
+                        MAKHUVUC= g.Key,
+                        SLBan = g.Count(c=>c.MABAN).
+                    }
+            )
+            return kv;
+        }
+        public bool sp_themkhuvuc(string tenkhuvuc)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@tenkhuvuc";
+            values[0] = tenkhuvuc;
+            return Execute("sp_themkhuvuc", name, values, parameter);
+        }
+        public bool sp_xoakhuvuc(int makhuvuc)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@makhuvuc";
+            values[0]= makhuvuc;
+            return Execute("sp_xoakhuvuc", name, values, parameter);
+        }
+        public List<KhuVucBanModels> sp_bandanhmuc(int makhuvuc)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@makhuvuc";
+            values[0] = makhuvuc;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_bandanhmuc",name,values,parameter));
+            return JsonConvert.DeserializeObject<List<KhuVucBanModels>>(json);
+        }
+        public bool sp_thembandanhmuc(string tenban,int makhuvuc)
+        {
+            int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@tenban";
+            values[0] = tenban;
+            name[1] = "@makhuvuc";
+            values[1] = makhuvuc;
+            return Execute("sp_thembandanhmuc", name, values, parameter);
+        }
+        public bool sp_xoabandanhmuc(int ban)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@maban";
+            values[0] = ban;
+            return Execute("sp_xoabandanhmuc", name, values, parameter);
+        }
+        public bool sp_suaban(int idban, string tenban)
+        {
+            int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0] = "@idban";
+            name[1] = "@tenban";
+            values[0] = idban;
+            values[1] = tenban;
+            return Execute("sp_suaban", name, values, parameter);
+        }
+        public bool sp_xoachitiethoadon(int idhoadon, int idban,int giamgia)
+        {
+            int parameter = 3;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@idhoadon";
+            name[1]= "@mathucdon";
+            name[2] = "@giamgia";
+            values[0]=idhoadon;
+            values[1]=idban;
+            values[2] = giamgia;
+            return Execute("sp_xoachitiethoadon", name, values, parameter);
+        }
+        public bool sp_xoahoadonkhihetmon(int idhoadon,int idban)
+        {
+            int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@idhoadon";
+            name[1] = "@idban";
+            values[0] = idhoadon;
+            values[1] = idban;
+            return Execute("sp_xoahoadonkhihetmon", name, values, parameter);
+        }
+        public List<KhuVucBanModels> sp_loadbantrangthaitrong(int makhuvuc)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@makhuvuc";
+            values[0]= makhuvuc;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_loadbantrangthaitrong",name,values,parameter));
+            return JsonConvert.DeserializeObject<List<KhuVucBanModels>>(json);
+        }
+        public List<KhuVucBanModels> sp_loadbantrangthaiconguoi(int makhuvuc)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@makhuvuc";
+            values[0] = makhuvuc;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_loadbantrangthaiconguoi",name,values,parameter));
+            return JsonConvert.DeserializeObject<List<KhuVucBanModels>>(json);
+        }
+        public bool sp_chuyenbantable(int mabanmoi, int mabancu)
+        {
+            int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@mabanmoi";
+            name[1]= "@mabancu";
+            values[0]=mabanmoi;
+            values[1]=mabancu;
+            return Execute("sp_chuyenbantable", name, values, parameter);
+        }
+        public List<KhuVucBanModels> sp_loadbancangop(int makhuvuc, int maban)
+        {
+            int parameter = 2;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@makhuvuc";
+            name[1]= "@maban";
+            values[0]=makhuvuc;
+            values[1]=maban;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_loadbancangop", name, values, parameter));
+            return JsonConvert.DeserializeObject<List<KhuVucBanModels>>(json);
+        }
+        public List<HoaDonModels> sp_loadcthdbangop(int mabangop)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@mabangop";
+            values[0]=mabangop;
+            string json = JsonConvert.SerializeObject(LoadDataParameter("sp_loadcthdbangop", name, values, parameter));
+            return JsonConvert.DeserializeObject<List<HoaDonModels>>(json);
+        }
+        public bool sp_xoahoadongopban(int maban)
+        {
+            int parameter = 1;
+            string[] name = new string[parameter];
+            object[] values = new object[parameter];
+            name[0]= "@maban";
+            values[0]= maban;
+            return Execute("sp_xoahoadongopban", name,values,parameter);
+        }
+        public List<KhuVucBanModels> sp_danhsachkhuvuccobantrong()
+        {
+            string json = JsonConvert.SerializeObject(LoadData("sp_danhsachkhuvuccobantrong"));
+            return JsonConvert.DeserializeObject<List<KhuVucBanModels>>(json);
+        }
+    }
+}
